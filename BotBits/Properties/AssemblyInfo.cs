193bdb7912e8c76b66bb564dd136cfcc6bff5d9b
Yonom <sepehr1376@gmail.com>
2015-02-16 11:08:52 +0100

    Fix compile error

--- BotBits/Properties/AssemblyInfo.cs
+++ BotBits/Properties/AssemblyInfo.cs
@@ -16,4 +16,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 
-[assembly: InternalsVisibleTo("BotBits.Tests")]
+//[assembly: InternalsVisibleTo("BotBits.Tests")]
