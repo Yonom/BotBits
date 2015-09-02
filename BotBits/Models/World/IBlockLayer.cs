f33c8584913feba8ea66cfb2564059391cd69beb
Yonom <sepehr1376@gmail.com>
2015-09-02 10:32:16 +0200

    Implement In, At, CopyWorld and SetWorld functions for Worlds

--- BotBits/Models/World/IBlockLayer.cs
+++ BotBits/Models/World/IBlockLayer.cs
@@ -2,7 +2,7 @@
 
 namespace BotBits
 {
-    public interface IBlockLayer<T> : IEnumerable<WorldItem<T>> where T : struct
+    public interface IBlockLayer<T> : IEnumerable<LayerItem<T>> where T : struct
     {
         int Height { get; }
         int Width { get; }
--- BotBits/Models/World/World.cs
+++ BotBits/Models/World/World.cs
@@ -1,4 +1,6 @@
-﻿using System.Diagnostics;
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace BotBits
 {
@@ -11,7 +13,8 @@ namespace BotBits
     }
 
     [DebuggerDisplay("Width = {Width}, Height = {Height}")]
-    public abstract class World<TForeground, TBackground> : IWorld<TForeground, TBackground> 
+    public abstract class World<TForeground, TBackground> 
+        : IWorld<TForeground, TBackground> , IWorldAreaEnumerable<TForeground, TBackground> 
         where TForeground : struct 
         where TBackground : struct
     {
@@ -21,6 +24,16 @@ namespace BotBits
             this.Width = width;
             this.Foreground = new BlockLayer<TForeground>(width, height);
             this.Background = new BlockLayer<TBackground>(width, height);
+        }
+        
+        public WorldItem<TForeground, TBackground> At(Point point)
+        {
+            return this.At(point.X, point.Y);
+        }
+
+        public WorldItem<TForeground, TBackground> At(int x, int y)
+        {
+            return new WorldItem<TForeground, TBackground>(this, x, y);
         }
 
         public BlockLayer<TBackground> Background { get; private set; }
@@ -38,5 +51,22 @@ namespace BotBits
 
         public int Height { get; private set; }
         public int Width { get; private set; }
+
+        public IEnumerator<WorldItem<TForeground, TBackground>> GetEnumerator()
+        {
+            return this.In(this.Area).GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+        World<TForeground, TBackground> IWorldAreaEnumerable<TForeground, TBackground>.World
+        {
+            get { return this; }
+        }
+
+        public Rectangle Area { get { return new Rectangle(0, 0, this.Width, this.Height); } }
     }
 }
