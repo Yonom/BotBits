016b3af9fa51b2925e72d3781c18200625a2da4b
Sepehr Farshid / Processor <sepehr1376@gmail.com>
2016-06-04 13:45:10 +0200

    Add canAddToCrews support

---
+++ BotBits/Models/Crew.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.RegularExpressions;
+
+namespace BotBits
+{
+    public class Crew
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+
+        public Crew(string id, string name)
+        {
+            this.Id = id;
+            this.Name = name;
+        }
+
+        public static Crew FromName(string name)
+        {
+            return new Crew(Regex.Replace(name, @"\s+", "").ToLower(), name);
+        }
+    }
+}
---
+++ BotBits/Packages/MessageHandler/Events/CanAddToCrewsEvent.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using PlayerIOClient;
+
+namespace BotBits.Events
+{
+    [ReceiveEvent("canAddToCrews")]
+    public sealed class CanAddToCrewsEvent : ReceiveEvent<CanAddToCrewsEvent>
+    {
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="CanAddToCrewsEvent" /> class.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        /// <param name="client"></param>
+        internal CanAddToCrewsEvent(BotBitsClient client, Message message)
+            : base(client, message)
+        {
+            this.Crews = new List<Crew>();
+            
+            for (uint i = 0; i <= message.Count - 1; i += 2)
+            {
+                var id = message.GetString(i);
+                var name = message.GetString(i + 1);
+
+                this.Crews.Add(new Crew(id, name));
+            }
+        }
+        public List<Crew> Crews { get; set; }
+    }
+}
