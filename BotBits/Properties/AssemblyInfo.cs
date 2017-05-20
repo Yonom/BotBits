1487d24be9866a2e58a5e427f937a5daa3edbdb6
Simon Farshid <Yonom@users.noreply.github.com>
2017-05-20 16:47:30 +0200

    Update assembly info

--- BotBits/Properties/AssemblyInfo.cs
+++ BotBits/Properties/AssemblyInfo.cs
@@ -6,13 +6,15 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 
-[assembly: AssemblyTitle("BotBits.Core")]
-[assembly: AssemblyProduct("BotBits.Core")]
+[assembly: AssemblyTitle("BotBits")]
+[assembly: AssemblyProduct("BotBits")]
 [assembly: Guid("fbac626f-42ca-4413-ba36-1263bb636cbf")]
 [assembly: AssemblyDescription("BotBits is a framework to help bot makers make bots faster.")]
-[assembly: AssemblyCopyright("Copyright © Sepehr Farshid 2015")]
+[assembly: AssemblyCopyright("Copyright © Simon Farshid 2017")]
 [assembly: AssemblyTrademark("BotBits")]
 [assembly: ComVisible(false)]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly:InternalsVisibleTo("BotBits.Tests")]
+#if DEBUG
+[assembly: InternalsVisibleTo("BotBits.Tests")]
+#endif
